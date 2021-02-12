68ea95466adf23e929ef99c65d76921a8e9a66d4
KISLAYA SRIVASTAVA <akshat.sri2605@gmail.com>
2021-02-12 14:07:16 +0530

    STEP 4

--- StringCalculator.java
+++ StringCalculator.java
@@ -5,12 +5,27 @@ public class StringCalculator {
 	static int Add(String s) {
 		if (s.length() == 0)
 			return 0;
-      
+      		
 		int res = 0;
-		String num[] = s.split("[,\n]");
-		for (String i : num) {
+		String nums[];
+		if (s.startsWith("//")) {
+			nums = startWithSlash(s);
+		} else {
+			nums = s.split("[,\n]");
+		}
+
+		for (String i : nums) {
 			res += Integer.parseInt(i);
 		}
 		return res;
 	}
+
+	public static String[] startWithSlash(String str) {
+		int idx = str.indexOf("\n");
+		String newstr = str.substring(idx + 1);
+		String pattern = str.substring(2, idx);
+		String nums[] = newstr.split(pattern);
+		return nums;
+	}
+
 }
